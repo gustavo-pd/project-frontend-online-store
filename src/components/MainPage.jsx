5db76fcf50c5493fc7c4fe43275cd11d09cc6f04
GustavoPD <gudalmoro@gmail.com>
2021-09-21 15:38:55 -0300

    Atualizando

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,42 +1,58 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { getCategories/* , getProductsFromCategoryAndQuery  */ } from '../services/api';
+import { getCategories, getProductsFromCategoryAndQuery } from '../services/api';
 import Categories from './Categories';
+import ProductList from './ProductList';
 
 export default class MainPage extends React.Component {
   constructor() {
     super();
     this.state = {
-      // categoriaId: '',
+      categoriaId: '',
       query: '',
       categoriaList: [],
+      productList: [],
     };
   }
 
+  filterList = (id) => {
+    this.setState({ categoriaId: id }, () => this.handleClick());
+    console.log(id);
+  }
+
   componentDidMount = async () => {
-    // const { categoriaId, query } = this.state;
     const fetchAPi = await getCategories();
     // const fetchItem = await getProductsFromCategoryAndQuery(query, categoriaId);
     this.categoriesList(fetchAPi);
+    this.handleClick();
   }
 
-  handleChange = ({ target: { value, id } }) => {
+  handleChange = async ({ target: { value, id } }) => {
     this.setState({ [id]: value });
   }
 
+  handleClick = async () => {
+    const { categoriaId, query } = this.state;
+    const fetchItem = await getProductsFromCategoryAndQuery(query, categoriaId);
+    if (fetchItem) this.setState({ productList: fetchItem.results });
+  }
+
   categoriesList(fetchAPi) {
-    console.log(fetchAPi);
     this.setState({ categoriaList: fetchAPi });
   }
 
   render() {
-    const { query, categoriaList } = this.state;
+    const { query, categoriaList, productList } = this.state;
     return (
       <div className="container pg-inicial">
         <div className="ui vertical text menu">
           <div className="header item">Categorias</div>
           { (categoriaList !== []) && categoriaList.map((categoria) => (
-            <Categories key={ categoria.id } categoria={ categoria } />
+            <Categories
+              key={ categoria.id }
+              categoria={ categoria }
+              onClick={ this.filterList }
+            />
           ))}
         </div>
         <div className="content">
@@ -44,11 +60,20 @@ export default class MainPage extends React.Component {
             <div className="ui icon input search-input">
               <i className="search left icon" />
               <input
+                data-testid="query-input"
                 type="text"
                 id="query"
                 value={ query }
                 onChange={ this.handleChange }
               />
+              <button
+                type="button"
+                data-testid="query-button"
+                onClick={ this.handleClick }
+              >
+                Pesquisar
+              </button>
+
             </div>
             <Link
               to="./shoppingcart"
@@ -57,9 +82,14 @@ export default class MainPage extends React.Component {
               <i className="shopping cart big icon" />
             </Link>
           </section>
-          { query === '' && (
+          { productList.length < 1 ? (
             <div data-testid="home-initial-message" className="default-text">
               Digite algum termo de pesquisa ou escolha uma categoria.
+            </div>
+          ) : (
+            <div data-testid="home-initial-message" className="default-text">
+              { productList.map((product) => (
+                <ProductList key={ product.id } product={ product } />))}
             </div>
           )}
         </div>
